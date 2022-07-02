19411a1711bd8a6e2fb29df4daf8f975140b481f
Kotiler <10221125+hyhcom@user.noreply.gitee.com>
2022-07-02 20:16:22 +0800

    ffdefefefe

--- src/main/java/com/example/UsedCarApplication.java
+++ src/main/java/com/example/UsedCarApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
+
 @MapperScan("com.example")//对包下所有的接口进行实现
 @SpringBootApplication//@SpringBootApplication  : 是Sprnig Boot项目的核心注解，目的是开启自动配置
 public class UsedCarApplication {
